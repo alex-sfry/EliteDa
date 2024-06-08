11427dbec90b5fb135b335ffed886e909c3a63d5
Aleksandr Tochilin <alex8079@gmail.com>
2024-06-08 18:21:34 +0300

    remove cookieValidationKey from config

--- config/web.php
+++ config/web.php
@@ -4,6 +4,8 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 $mail = require __DIR__ . '/mail_config.php';
 
+require __DIR__ . '/cvk.php';
+
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
@@ -72,7 +74,7 @@ $config = [
         ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'y6-kI8DCYXsT-N2nIJALHgB291FX8bGO',
+            'cookieValidationKey' => $cvk,
             'parsers' => [
                 'application/json' => 'yii\web\JsonParser',
             ],
