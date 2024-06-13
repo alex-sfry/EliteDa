79dd2d6907450518af05406e4edce9419a71dd2d
Aleksandr Tochilin <alex8079@gmail.com>
2024-06-13 22:45:12 +0300

    add create_rings_table migration

---
+++ migrations/m240613_153351_create_rings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%rings}}`.
+ */
+class m240613_153351_create_rings_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%rings}}', [
+            'name' => $this->string(255),
+            'type' => $this->string(50),
+            'system_name' => $this->string(255),
+            'distance_to_arrival' => $this->string(50),
+            'body_name' => $this->string(255),
+            'reserve' => $this->string(50),
+            'timestamp' => $this->string(50),
+        ]);
+
+        $this->addPrimaryKey('pk_rings', '{{%rings}}', 'name');
+        $this->createIndex('idx_system_name', '{{%rings}}', 'system_name');
+        $this->createIndex('idx_ring_type', '{{%rings}}', 'type');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%rings}}');
+    }
+}
