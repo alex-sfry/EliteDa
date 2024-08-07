aa7b89b349604e7e54896da4e562a675a0f50b27
Aleksandr Tochilin <alex8079@gmail.com>
2024-08-07 10:33:06 +0300

    fix redirect after login

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -47,8 +47,9 @@ class UserController extends Controller
         $model = new LoginForm();
         $model->referer = isset(Yii::$app->request->referrer) ? Yii::$app->request->referrer : null;
         if ($model->load(Yii::$app->request->post())) {
-            if ($model->login() && Yii::$app->request->hostName === 'elida') {
-                return $this->redirect(['admin/index']);
+            if ($model->login() && Yii::$app->request->hostName === HOST_NAME) {
+                return Yii::$app->user->getIdentity()->username === ADMIN ? $this->redirect(['admin/index']) :
+                    $this->goHome();
             }
         }
 
